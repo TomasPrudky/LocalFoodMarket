144d2ddf60c222dbe24c1dd6a4136674a438f9b4
Tomas Prudky <prudky.tomas@email.cz>
2023-10-10 20:34:27 +0200

    init commit

---
+++ farmersmarket-be/src/main/java/cs/prudkytomas/farmersmarketbe/FarmersMarketBackendApplication.java
@@ -0,0 +1,13 @@
+package cs.prudkytomas.farmersmarketbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmersMarketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmersMarketBackendApplication.class, args);
+	}
+
+}
---
+++ farmersmarket-be/src/test/java/cs/prudkytomas/farmersmarketbe/FarmersMarketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.prudkytomas.farmersmarketbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmersMarketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
